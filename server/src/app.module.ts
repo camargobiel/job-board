d12a0970ac18c2e996c9f7cbe73fe7b86d4bb48a
camargobiel <camargoobiel@gmail.com>
2023-12-28 15:05:26 -0300

    feat: 🎸 basic structure done

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ControllersModule } from './controllers/controllers.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ControllersModule, ServicesModule],
 })
 export class AppModule {}
